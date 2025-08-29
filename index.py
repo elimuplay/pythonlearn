2edb02a5833d7689fa34c4dd74bd7c46ffa5ba37
elimuplay <=songoroapps@gmail.com>
2025-08-29 15:04:43 +0300

    First Commit

---
+++ index.py
@@ -0,0 +1,13 @@
+site_name = "mfalme site"
+#a,b,c,d= 'A','B','C','D'
+#print(site_name)
+#print('cap of a is ',a)
+#my_children = ['Raiyan', 'Yahya', 'Aisha', 'Khairat']
+#print('my first child is :', my_children[0]);
+
+#my_wives = {'first':'Halima','Second':'Aisha','Third':'Khadija'}
+#print('my first wife is :', my_wives['first']);
+married = True
+#print(not(married))
+if married:
+    print('I am married')
